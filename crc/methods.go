791aff717456733e6bf8f4730b2c861feac04bb1
Marcin Borowicz <marcinbor85@gmail.com>
2018-07-04 21:19:08 +0200

    poprawione bledy dot. tablic

--- crc/methods.go
+++ crc/methods.go
@@ -17,23 +17,24 @@ func AddDoubleCrc32(mem *gohex.Memory, startAdr uint32, endAdr uint32, pad byte)
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	data := mem.ToBinary(startAdr, endAdr - startAdr, pad)
-
+	
 	a := make([]byte, 4)
-
 	binary.LittleEndian.PutUint32(a, Crc32UpdateBlock(0, data))
 	err := mem.AddBinary(endAdr, a)
 	if err != nil {
 		return err
 	}
 
+	a = make([]byte, 4)
 	binary.LittleEndian.PutUint32(a, (uint32)(rand.Intn(0xFFFFFFFF)))
-	mem.AddBinary(endAdr+4, a)
+	err = mem.AddBinary(endAdr+4, a)
 	if err != nil {
 		return err
 	}
 
-	binary.LittleEndian.PutUint32(a, Crc32UpdateBlock(0, a))
-	mem.AddBinary(endAdr+8, a)
+	b := make([]byte, 4)
+	binary.LittleEndian.PutUint32(b, Crc32UpdateBlock(0, a))
+	err = mem.AddBinary(endAdr+8, b)
 	if err != nil {
 		return err
 	}
